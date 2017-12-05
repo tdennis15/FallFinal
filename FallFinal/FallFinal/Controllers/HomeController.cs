36d3b1ed8f337d2a70b3932481b51679d4f265f5
TJ Dennis <tdennis15@mail.wou.edu>
2017-12-05 15:39:45 -0800

    added a few items in the view and in the controller

--- FallFinal/FallFinal/Controllers/HomeController.cs
+++ FallFinal/FallFinal/Controllers/HomeController.cs
@@ -3,28 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FallFinal.Models;
 
 namespace FallFinal.Controllers
 {
     public class HomeController : Controller
     {
+        //adding in our context class for the use in the controller
+        private AuctionContext db = new AuctionContext();
+
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        
+        public ActionResult Items()
         {
-            ViewBag.Message = "Your application description page.";
+            var item = db.Items;
+            return View(item);
+        }
 
+
+
+        /// <summary>
+        /// Here is the shell for the get method in the controller for the CRUD create page
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult CreateItem()
+        {
             return View();
         }
 
-        public ActionResult Contact()
+        /// <summary>
+        /// Here is the post for the CRUD method of create in our application
+        /// We have a form passed in from the html page so that we can use elements to 
+        /// identify pieces of the data to put into our database and so that the form 
+        /// validation because simpler.
+        /// 
+        /// </summary>
+        /// <param name="collection">just a simple form</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult CreateItem(FormCollection collection)
         {
-            ViewBag.Message = "Your contact page.";
+            try
+            {
+                Item item = db.Items.Create();
 
-            return View();
+                item.I_Name = collection["itemName"];
+                return RedirectToAction("Index");
+            }
+
+            catch
+            {
+                return View("Index");
+            }
         }
     }
 }
